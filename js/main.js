3129e2febea4dd3f832e272e744aec924b4483f2
Sergey48n <s19cn@mail.ru>
2022-01-15 16:30:48 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,35 +1,37 @@
 var hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
-  
+
   loop: true,
   speed: 2000,
-
-
 
   // Navigation arrows
   navigation: {
     nextEl: '.hotel-slider__button--next',
     prevEl: '.hotel-slider__button--prev',
   },
- 
-  effect: 'coverflow', 
-  
+
+  effect: 'coverflow',
+
 });
 
 var reviewsSlider = new Swiper('.reviews-slider', {
   // Optional parameters
-  
+
   loop: true,
   speed: 1000,
-
-
 
   // Navigation arrows
   navigation: {
     nextEl: '.reviews-slider__button--next',
     prevEl: '.reviews-slider__button--prev',
   },
- 
-  
-  
+
+
+
 });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log('Клик по кнопке меню');
+  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+}); 
